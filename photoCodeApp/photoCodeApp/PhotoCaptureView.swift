344aafa29aa3d2558286dee74e459a2be17f20a6
ZacharyEbaugh <69406106+ZacharyEbaugh@users.noreply.github.com>
2021-11-11 04:34:08 -0500

    camera working, compiler started
    camera preview working 
    picture and retake work but no action with picture
    compiler textbox exits when enter is pressed
    language drop menu started

---
+++ photoCodeApp/photoCodeApp/PhotoCaptureView.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCaptureView.swift
+//  photoCodeApp
+//
+//  Created by Zachary Ebaugh on 11/11/21.
+//
+
+
+import SwiftUI
+
+struct PhotoCaptureView: View {
+    
+    @Binding var showImagePicker: Bool
+    @Binding var image: Image?
+    
+    var body: some View {
+        ImagePicker(isShown: $showImagePicker, image: $image)
+    }
+}
+
+#if DEBUG
+struct PhotoCaptureView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCaptureView(showImagePicker: .constant(false), image: .constant(Image("")))
+    }
+}
+#endif
