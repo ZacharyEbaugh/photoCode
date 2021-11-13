b7bc2e792b3b1be23a02937361fe8ce5d1574304
ZacharyEbaugh <69406106+ZacharyEbaugh@users.noreply.github.com>
2021-11-13 04:10:41 -0500

    dropdown, textbox
    dropdown for language choice
    textbox for compiler allows return key without exiting

---
+++ photoCodeApp/photoCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoCodeAppApp.swift
+//  photoCodeApp
+//
+//  Created by Zachary Ebaugh on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct photoCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
