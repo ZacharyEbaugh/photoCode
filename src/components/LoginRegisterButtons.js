40ee808fb8b8905c635d92aa2c5a3b3c32c19812
Zachary Licong Ebaugh <69406106+ZacharyEbaugh@users.noreply.github.com>
2022-07-05 13:38:13 -0400

    Add files via upload

---
+++ src/components/LoginRegisterButtons.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import LoginScreen from './Login';
+import RegisterScreen from './Register';
+import Splash from './SplashPage';
+
+import 
+    { 
+        View, 
+        Text, 
+        Pressable, 
+        StyleSheet,    
+    } from 'react-native';
+
+const actionNames = ['Login', 'Register'];
+
+import { useNavigation } from '@react-navigation/native';
+
+function GoToButton({ screenName, action }) {
+    const navigation = useNavigation();
+
+    return (
+        <Pressable
+            onPress={() => navigation.navigate(screenName)}
+        >
+            <Text style={styles.buttons}>
+                {actionNames[action]}
+            </Text>
+        
+        </Pressable>
+
+    );
+}
+
+
+class LoginRegister extends React.Component {
+    render() {
+        return (
+            <View style = {styles.container}>
+                <View>
+                    <GoToButton screenName={LoginScreen} action={0}/>
+                </View>
+                <View>
+                    <GoToButton screenName={RegisterScreen} action={1}/>
+                </View>
+            </View>
+        );
+    };
+}
+
+const styles = StyleSheet.create ({
+    container: {
+        // backgroundColor: 'black',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 460,
+    },
+
+    buttons: {
+        height: 80,
+        width: 300,
+        fontSize: 40,
+        backgroundColor: '#0066FF',
+        color: 'white',
+        // fontFamily: 'JetBrainsMono-Regular',
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 14,
+        padding: 10,
+        textAlign: 'center',
+        marginBottom: 20,
+        zIndex: 9999,
+        elevation: 9999,
+    },
+});
+
+
+export default LoginRegister;
---
+++ src/components/Login_SignUpButtons.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+import HomeScreen from './LoginRegisterButtons';
+import Register from './Register';
+
+
+import {
+    View,
+    Text,
+    Pressable,
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const actionNames = ['Login', 'Sign Up'];
+
+import { useNavigation } from '@react-navigation/native';
+
+function LoginButton({ screenName }) {
+    const navigation = useNavigation();
+
+    return (
+        <Pressable style={styles.LoginButton}
+            onPress={() => navigation.navigate(screenName)}
+        >
+            <Text style={styles.loginText}>
+                Login
+            </Text>
+
+        </Pressable>
+
+    );
+}
+
+function SignUpButton({ screenName }) {
+    const navigation = useNavigation();
+
+    return (
+        <Pressable style={styles.SignUpButton}
+            onPress={() => navigation.navigate(screenName)}
+        >
+            <Text style={styles.signUpText}>
+                Sign Up
+            </Text>
+
+        </Pressable>
+
+    );
+}
+
+
+class Login_SignUp extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <LoginButton screenName={HomeScreen} action={0} />
+                </View>
+                <View style={styles.line}></View>
+                <View>
+                    <SignUpButton screenName={Register} action={1} />
+                </View>
+            </View>
+        );
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        // backgroundColor: 'black',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+
+    LoginButton: {
+        height: 80,
+        width: 300,
+        backgroundColor: '#0066FF',
+        borderRadius: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        zIndex: 9999,
+        elevation: 9999,
+    },
+
+    SignUpButton: {
+        height: 80,
+        width: 300,
+        backgroundColor: 'white',
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        zIndex: 9999,
+        elevation: 9999,
+    },
+
+    loginText: {
+        fontSize: 40,
+    },
+
+    line: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        width: windowWidth * 0.8,
+        marginBottom: 10,
+    },
+
+    signUpText: {
+        fontSize: 40,
+    }
+});
+
+
+export default Login_SignUp;
