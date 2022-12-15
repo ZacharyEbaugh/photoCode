ea95d1ceb51e4157dde83c24d7411690f48bae5d
Brandon Spangler <94080573+brandonspangler2@users.noreply.github.com>
2022-12-15 16:03:38 -0500

    Update index.js

--- Web/server/index.js
+++ Web/server/index.js
@@ -8,9 +8,6 @@ const crypt = require('bcrypt');
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:3000"
-}));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
