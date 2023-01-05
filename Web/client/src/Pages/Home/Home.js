037db1d00781c3b752222e5040ddb9399a1a84aa
Zachary Licong Ebaugh <69406106+ZacharyEbaugh@users.noreply.github.com>
2023-01-05 11:43:22 -0500

    Update Home.js

--- Web/client/src/Pages/Home/Home.js
+++ Web/client/src/Pages/Home/Home.js
@@ -40,6 +40,7 @@ function Home(props) {
 
     // Get all projects for user from database
     useEffect(() => {
+        console.log(localStorage);
         const user_id = localStorage.getItem('user_id');
         console.log(user_id);
         axios.get(`https://photocode.app:8443/getAllProjects?user_id=${user_id}`)
